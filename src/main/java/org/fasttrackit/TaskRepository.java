c861b5b96995e86749ac8345696157cc2c3ecc11
”Cristi <cristi.campean@hotmail.com>
2020-07-26 23:13:50 +0300

    added CreateTaskRequest in Task repository

--- src/main/java/org/fasttrackit/TaskRepository.java
+++ src/main/java/org/fasttrackit/TaskRepository.java
@@ -2,6 +2,7 @@ package org.fasttrackit;
 
 import org.fasttrackit.config.DatabaseConfiguration;
 import org.fasttrackit.transfer.CreateTaskRequest;
+import org.fasttrackit.transfer.UpdateTaskRequest;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -10,15 +11,33 @@ import java.sql.SQLException;
 public class TaskRepository {
 
     public void createTask(CreateTaskRequest request) throws SQLException {
-         String sql = "INSERT INTO task (description, deadline) VALUES( ? ,?)";
+        String sql = "INSERT INTO task (description, deadline) VALUES( ? ,?)";
         // try with resorceous
-         try(
-            PreparedStatement preparedStatement = DatabaseConfiguration.getConnection().prepareStatement(sql)){
-            preparedStatement.setString(1,request.getDescription());
+        try (
+                PreparedStatement preparedStatement = DatabaseConfiguration.getConnection().prepareStatement(sql)) {
+            preparedStatement.setString(1, request.getDescription());
             preparedStatement.setDate(2, Date.valueOf(request.getDeadline()));
             preparedStatement.executeUpdate();
         }
 
+    }
+
+    public void deleteTask(long id) throws SQLException {
+        String sql = "DELITE FROM task WHERE id =?";
+        try (PreparedStatement preparedStatement = DatabaseConfiguration.getConnection().prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        }
+
+    }
+
+    public void updateTask(long id, UpdateTaskRequest request) throws SQLException {
+        String sql = "UPDATE task SET done =? WHERE  id=?";
+        try (PreparedStatement preparedStatement = DatabaseConfiguration.getConnection().prepareStatement(sql)) {
+            preparedStatement.setBoolean(1, request.isDone());
+            preparedStatement.setLong(2, id);
+            preparedStatement.executeUpdate();
+        }
     }
 }
 
